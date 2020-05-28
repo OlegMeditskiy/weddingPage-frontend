ac6706524647b0ae1c5752ba2c947462bbc28292
OlegMeditskiy <Gf8594njvk>
2020-05-28 21:13:52 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './service-worker';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
